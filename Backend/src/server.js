592498adec7cfd6e47cf656c64574155110c0909
MeetRamatri <meetramatri12@gmail.com>
2026-02-24 15:23:39 +0530

    Connecting MongoDB

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/.env' });
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +14,12 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Server is running normally.');
 });
+
+// Database Connection
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB successfully!'))
+    .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 // Start Server
 app.listen(PORT, () => {
